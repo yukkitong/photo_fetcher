19b42738d89cd6ba8c3c76a70a0cbbfa7ac5d5ba
Jaesung BAE <jason98.bae@meatple.com>
2022-07-03 16:02:24 +0900

    (feature) add  photo list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'photo.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,57 +11,135 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomeState createState() => _MyHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomeState extends State<MyHome> {
+  List<Photo> photos = [];
+  bool loading = false;
+  int albumId = 1;
+  final ScrollController _scrollController = ScrollController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPhotos(albumId);
+    _scrollController.addListener(() {
+      print(_scrollController.position.maxScrollExtent);
+      print(_scrollController.position.pixels);
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        albumId++;
+        getPhotos(albumId);
+      }
     });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> getPhotos(int albumId) async {
+    if (albumId > 10) {
+      return;
+    }
+
+    final String url =
+        'https://jsonplaceholder.typicode.com/photos?albumId=$albumId';
+    try {
+
+      if (albumId == 1) {
+        setState(() {
+          loading = true;
+        });
+      }
+
+      http.Response response = await http.get(Uri.parse(url));
+      if (albumId == 1) {
+        setState(() {
+          loading = false;
+        });
+      }
+
+      final items = json.decode(response.body);
+      items.forEach((item) {
+        photos.add(Photo.fromJson(item));
+      });
+
+      // 상단의 photos.add(Photo.fromJson(item)) 에서 받아온 데이터를 다시 화면에 리빌드 하기 위함.
+      setState(() {});
+
+    } catch (err) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.red[200],
+            title: const Text('Failure'),
+            content: const Text('Fail to get album data'),
+          );
+        },
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Photo Fetcher'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: loading
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        controller: _scrollController,
+        itemCount: photos.length + 1,
+        itemBuilder: (context, index) {
+          if (index == photos.length) {
+            return const Center(
+              child: Padding(
+                padding: EdgeInsets.only(top: 15.0),
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          return Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Image.network(
+                    photos[index].url,
+                    width: double.infinity,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  Text(photos[index].title)
+                ],
+              ),
+              Text(
+                '${index + 1}',
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 60),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
---
+++ lib/photo.dart
@@ -0,0 +1,35 @@
+class Photo {
+  int albumId;
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
+
+  Photo({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+        albumId: json['albumId'],
+        id: json['id'],
+        title: json['title'],
+        url: json['url'],
+        thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['albumId'] = albumId;
+    data['id'] = id;
+    data['title'] = title;
+    data['url'] = url;
+    data['thumbnailUrl'] = thumbnailUrl;
+    return data;
+  }
+}
